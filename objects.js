dca0ab7a11f47f9d588389531c66cab4720ddc06
Diego Z <diego.zegarra78@gmail.com>
2018-08-10 23:23:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,6 +5,6 @@ function updatePlaylist (playlist,artistName,songTitle){
 }
 
 function removeFromPlaylist (playlist,artistName){
-   delete playlist.ruby
-   return playlist
+   return delete playlist.ruby
+  
 }

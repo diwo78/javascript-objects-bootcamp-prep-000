a9c78d9cb8d696a34f4adfabbe19a9aad9d040b7
Diego Z <diego.zegarra78@gmail.com>
2018-08-10 23:56:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ function updatePlaylist (playlist,artist,song){
  playlist.artist = "My Bloody Valentine"
  return playlist
 }
+function removeFromPlaylist (playlist,artist){
+  delete playlist.artist
+  return playlist
+}

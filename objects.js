2915b31996de8372b26d890eff2ee299d31f6938
Diego Z <diego.zegarra78@gmail.com>
2018-08-10 23:14:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist = { artistName: "songTitle"}
 function updatePlaylist (playlist,artistName,songTitle){
-  return Object.assing({},playlist,{[ruby]: cervercero})
+  return Object.assing({},playlist,{[artistName]: songTitle})
 }
